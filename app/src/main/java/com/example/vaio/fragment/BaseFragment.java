9f682ae0b15a45c83001da94697dcbde11a561a7
vietcoscc <vietcoscc@gmail.com>
2016-11-29 00:46:02 +0700

    Hoàn thành search

--- app/src/main/java/com/example/vaio/fragment/BaseFragment.java
+++ app/src/main/java/com/example/vaio/fragment/BaseFragment.java
@@ -121,7 +121,7 @@ public class BaseFragment extends Fragment implements AbsListView.OnScrollListen
                     isCleared = true;
                 }
                 arrItemListView.addAll((Collection<? extends ItemListView>) msg.obj);
-                database.insertArrItemListView((ArrayList<ItemListView>) msg.obj);
+                database.insertArrItemListView((ArrayList<ItemListView>) msg.obj,MyDatabase.TB_NAME_LIST_MAIN);
                 listViewAdapter.notifyDataSetChanged();
                 gridViewAdapter.notifyDataSetChanged();
             }
