24835521a0e8c6192dc30667ad2bbaccd1d35b21
TungMaiUet <mtung280196@gmail.com>
2016-11-28 20:43:23 +0700

    xong phan hoi

---
+++ app/src/main/java/com/example/vaio/dialog/FeedbackDialog.java
@@ -0,0 +1,73 @@
+package com.example.vaio.dialog;
+
+import android.Manifest;
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.DialogFragment;
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.vaio.learnmoregame.R;
+
+import java.util.regex.Pattern;
+
+/**
+ * Created by TungMai on 28/11/2016.
+ */
+
+public class FeedbackDialog extends DialogFragment implements View.OnClickListener {
+
+    private static final String TAG = "FeedbackDialog";
+    private View view;
+    private EditText edtInput;
+    private Button btnOk;
+    private Button btnCancel;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.dialog_feedback, container, false);
+        getDialog().setTitle("Phản hồi");
+        initViews();
+        return view;
+    }
+
+    private void initViews() {
+        edtInput = (EditText) view.findViewById(R.id.edt_input);
+        btnCancel = (Button) view.findViewById(R.id.btn_cancel);
+        btnOk = (Button) view.findViewById(R.id.btn_ok);
+        btnCancel.setOnClickListener(this);
+        btnOk.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_cancel:
+                getDialog().dismiss();
+                break;
+            case R.id.btn_ok:
+                String input = edtInput.getText().toString();
+                if (input.isEmpty() || input.trim().isEmpty()) {
+                    Toast.makeText(getDialog().getContext(), "Bạn chưa nhập phản hồi", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                getDialog().dismiss();
+                break;
+        }
+    }
+}
